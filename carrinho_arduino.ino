1d28487a1c4c9776f852cee78c1783206e33e943
Tiago Lopes <52180428+Tiago0Br@users.noreply.github.com>
2023-05-29 19:44:12 -0300

    Corrigindo movimentação para trás

--- carrinho_arduino.ino
+++ carrinho_arduino.ino
@@ -32,10 +32,10 @@ void movFrente() {
 }
 
 void movTras() {
-  digitalWrite(IN1, HIGH);
-  digitalWrite(IN2, LOW);
-  digitalWrite(IN3, HIGH);
-  digitalWrite(IN4, LOW);
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
   analogWrite(ENA, VEL);
   analogWrite(ENB, VEL);
 }
